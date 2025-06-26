132994e4361f6ec5c340b39194c1f6b08604ae33
Aaliyan Ahmed <170867862+aaliyanahmed1@users.noreply.github.com>
2025-06-27 02:43:30 +0500

    Add files via upload

---
+++ problem5-1.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+
+# Load the base model (you can choose yolov8n.pt, yolov8s.pt, etc.)
+model = YOLO('yolo12n.pt')
+
+# Fine-tune on custom dataset
+model.train(
+    data='processed_yolo_dataset/data.yaml',  # Your custom dataset YAML path
+    epochs=30,
+    imgsz=640,
+    batch=16,
+    lr0=0.001,             # initial learning rate
+    optimizer='SGD',       # 'SGD', 'Adam', or 'AdamW'
+    momentum=0.9,          # used with SGD
+    weight_decay=0.0005,   # L2 regularization
+    warmup_epochs=3,       # number of warmup epochs
+    workers=2              # number of dataloader workers
+)
+
+#
